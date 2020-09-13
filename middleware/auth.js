5486a74f7b34a419ba3807089b5dc1eb91c764d3
Adnan <adnan172203@gmail.com>
2020-09-13 10:17:25 +0600

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+module.exports.auth = async (req, res, next) => {
+  if (req.signedCookies) {
+    //accessing token
+    const token = req.signedCookies['auth'];
+
+    try {
+      //verify token
+      const decoded = jwt.verify(token, 'secretKey');
+
+      //Getting user
+      const user = await User.findById(decoded.id);
+
+      req.user = user;
+
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'unauthorized token' });
+    }
+  } else {
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
