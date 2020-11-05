7001b16f9e37632e289f6e376fa74d7cb0d38b1f
Adnan <adnan172203@gmail.com>
2020-11-05 15:42:36 +0600

    added shipping schema on order model

---
+++ models/Order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { ObjectId } = Schema;
+
+const shippingSchema = {
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+};
