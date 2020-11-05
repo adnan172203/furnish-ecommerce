f03ee0c1432463c7189ab86743e86b410ff397bc
Adnan <adnan172203@gmail.com>
2020-11-05 16:36:58 +0600

    added payment schema and order item schema on order model

--- models/Order.js
+++ models/Order.js
@@ -20,3 +20,34 @@ const shippingSchema = {
     required: true,
   },
 };
+
+const paymentSchema = {
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+};
+
+const orderItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+});
