ce014916fe91d3fe8eb9e6b5259b639334140e08
Adnan <adnan172203@gmail.com>
2020-11-17 07:41:53 +0600

    installed react and other packages for frontend

--- models/Order.js
+++ models/Order.js
@@ -67,9 +67,8 @@ const orderSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-)
+);
 
-
-const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.model('Order', orderSchema);
 
 module.exports = Order;
