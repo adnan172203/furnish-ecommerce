52225b35977115bfe0bbde8a3e20c9bb8293f877
Adnan <adnan172203@gmail.com>
2020-12-03 08:33:56 +0600

    replaced css with css modules on order list components

--- client/src/components/order-list/order-list.js
+++ client/src/components/order-list/order-list.js
@@ -3,31 +3,45 @@ import React from 'react';
 //css
 import Styles from './order-list.module.css';
 
+const {
+  order_list,
+  order_container_table100,
+  order_wrap_table100,
+  order_table100,
+  order_table100_head,
+  order_column1,
+  order_column2,
+  order_column3,
+  order_column4,
+  order_column5,
+  order_column6,
+} = Styles;
+
 const OrderList = () => {
   return (
-    <div className='order-list'>
-      <div className='order-container-table100'>
-        <div className='order-wrap-table100'>
-          <div className='order-table100'>
+    <div className={order_list}>
+      <div className={order_container_table100}>
+        <div className={order_wrap_table100}>
+          <div className={order_table100}>
             <table>
               <thead>
-                <tr className='order-table100-head'>
-                  <th className='order-column1'>Date</th>
-                  <th className='order-column2'>Order ID</th>
-                  <th className='order-column3'>Product</th>
-                  <th className='order-column4'>Paid</th>
-                  <th className='order-column5'>Quantity</th>
-                  <th className='order-column6'>Total</th>
+                <tr className={order_table100_head}>
+                  <th className={order_column1}>Date</th>
+                  <th className={order_column2}>Order ID</th>
+                  <th className={order_column3}>Product</th>
+                  <th className={order_column4}>Paid</th>
+                  <th className={order_column5}>Quantity</th>
+                  <th className={order_column6}>Total</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td className='order-column1'>2017-09-29 01:22</td>
-                  <td className='order-column2'>200398</td>
-                  <td className='order-column3'>iPhone X 64Gb Grey</td>
-                  <td className='order-column4'>$999.00</td>
-                  <td className='order-column5'>1</td>
-                  <td className='order-column6'>$999.00</td>
+                  <td className={order_column1}>2017_09_29 01:22</td>
+                  <td className={order_column2}>200398</td>
+                  <td className={order_column3}>iPhone X 64Gb Grey</td>
+                  <td className={order_column4}>$999.00</td>
+                  <td className={order_column5}>1</td>
+                  <td className={order_column6}>$999.00</td>
                 </tr>
               </tbody>
             </table>
