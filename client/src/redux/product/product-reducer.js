9bf6dad5f3a29c4a725ecbdb137e57347ee37048
Adnan <adnan172203@gmail.com>
2020-12-07 10:49:08 +0600

    made create product reducer and added into root reducer

--- client/src/redux/product/product-reducer.js
+++ client/src/redux/product/product-reducer.js
@@ -1,6 +1,8 @@
 import {
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
 } from './product-types';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -12,6 +14,23 @@ export const productListReducer = (state = { products: [] }, action) => {
         products: payload.products,
       };
     case PRODUCT_LIST_FAIL:
+      return {
+        error: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productCreateReducer = (state = {}, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case PRODUCT_CREATE_SUCCESS:
+      return {
+        product: payload,
+      };
+    case PRODUCT_CREATE_FAIL:
       return {
         error: payload,
       };
--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import {
   productListReducer,
+  productCreateReducer,
 } from './product/product-reducer';
 
 export default combineReducers({
   products: productListReducer,
+  productCreate: productCreateReducer,
 });
