0504b9e48456c59a2dfe691498e2a19b5db4217e
Adnan <adnan172203@gmail.com>
2020-12-11 07:46:50 +0600

    added shop product component into shop page

---
+++ client/src/pages/shop/components/shopProducts/ShopProduct.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+const ShopProduct = () => {
+  return (
+    <div>
+      <div class='product'>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+        <div class='product-item'>
+          <div class='product-img'>
+            <a href='#'>
+              <img src='https://picsum.photos/seed/picsum/200/300' alt='' />
+            </a>
+          </div>
+          <div class='product-desc'>
+            <h3 class='product-name'>white comfy</h3>
+            <p class='product-price'>$570</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopProduct;
--- client/src/pages/shop/shop.js
+++ client/src/pages/shop/shop.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Banner from '../../components/banner/banner';
+import ShopProduct from './components/shopProducts/ShopProduct';
 
 const Shop = () => {
   return (
     <>
       <Banner title='Shop' breadCrumb='Shop' />
+      <section class='main-shop'>
+        <div class='container'>
+          <div class='main-product'>
+            <ShopProduct />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
