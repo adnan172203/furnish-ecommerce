7847d897f90c1d6445553cd2f66db7921bb58b8a
Adnan <adnan172203@gmail.com>
2021-01-06 11:24:27 +0600

    place order process completed

--- client/src/pages/placeOrder/PlaceOrder.js
+++ client/src/pages/placeOrder/PlaceOrder.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import OrderItems from './orderItems/OrderItems';
 import ShippingAddress from './shippingAddress/ShippingAddress';
+import { createOrder } from '../../redux/order/orderAction';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+//css
+import Styles from './PlaceOrder.module.css';
+
+const { place_order_button,button_position } = Styles;
 
 const PlaceOrder = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cartReducer);
+
+  const placeOrderHandler = () => {
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+      })
+    );
+  };
+
   return (
     <>
       <ShippingAddress />
       <OrderItems />
+      <div className={button_position}>
+        <button
+          className={place_order_button}
+          type='submit'
+          onClick={placeOrderHandler}
+        >
+          Place Order
+        </button>
+      </div>
     </>
   );
 };
