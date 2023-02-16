4e39133b5da8650afec7c721233112d20f3ea785
adnan <adnan172203@gmail.com>
2023-02-16 12:38:52 +0600

    added new baseUrl

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,2 +1,6 @@
-let baseUrl = 'http://localhost:5000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://furnish-ecommerce.vercel.app'
+    : 'http://localhost:5000';
+
 export default baseUrl;
